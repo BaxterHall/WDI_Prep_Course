c30e23e210762087ffda6beccfb845cbf2304f8c
Baxter Hall <abaxterhall@beezy.local>
2016-12-19 22:24:28 -0500

    complete

--- Challenge_5/challenge.js
+++ Challenge_5/challenge.js
@@ -1,7 +1,28 @@
+//Excercise 1
 var name = "Baxter";
 var greeting = "Hello " + name;
   console.log(greeting)
-
-  var x = 1125
-  var awnser = x%2
-    console.log (awnser)  
+//Excercise 2
+var x = 1125
+var awnser = x%2
+    console.log(awnser)  
+//Excercise 3
+var a = "True";
+var b = "False";
+var c = "True";
+    console.log(a || b || c)
+//Excercise 4
+var a = "True";
+var b = "False";
+var c = "True"
+    console.log(a && b && c)
+//Excercise 5
+var answer1 = 10 - 3 * 5/4 + 202 % 3   
+var answer2 = 10 - (3 * (5/4) + (202 % 3)) 
+    console.log(answer1 , answer2)
+//Excercise 6
+String(12)
+parseInt("113")  
+parseFloat(1.12356) 
+x = parseInt("Hello World");
+    console.log(x)
